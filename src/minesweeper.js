d8f7c676b72dcebbc87f707d25d9799ac55bfd77
Chanthy Ngin <nginchanthy@gmail.com>
2017-12-09 22:04:37 -0800

    Adding bomb placing position

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -16,22 +16,28 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
   for(let rowIndex = 0; rowIndex < numberOfRows; rowIndex++){
     const row = [];
     for(let colIndex = 0; colIndex < numberOfColumns; colIndex++){
-      row.push(' ');
+      row.push(null);
     }
     board.push(row);
   }
 
   let numberOfBombsPlaced = 0;
+  const placedBombPosition = []; // Store the position of the placed bombs
+
   while (numberOfBombsPlaced < numberOfBombs) {
     const randomeRowIndex = Math.floor(Math.random() * numberOfRows);
     const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
-    board[randomeRowIndex][randomColumnIndex] = 'B';
+    //If there is not any bombs in this position
+    //
+    if (placedBombPosition.findIndex(p => p.join('-') === randomeRowIndex + '-' + randomColumnIndex) === -1) {
+      board[randomeRowIndex][randomColumnIndex] = 'B';
+      placedBombPosition.push([randomeRowIndex, randomColumnIndex]);
+      numberOfBombsPlaced++;
+    }
 
-    numberOfBombsPlaced++;
   }
 
   return board;
-
 };
 
 const printBoard = board => {
@@ -40,6 +46,7 @@ const printBoard = board => {
 
 const playerBoard = generatePlayerBoard(3, 3);
 const bombBoard = generateBombBoard(3,3,2);
-
+console.log('Player board:');
 console.log(printBoard(playerBoard));
+console.log('Bomb board:');
 console.log(printBoard(bombBoard));
